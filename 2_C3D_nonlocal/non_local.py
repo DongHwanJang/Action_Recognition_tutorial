1642841194a9baf70956edc2e7040e41607f409c
root <sunutf@gmail.com>
2021-07-13 08:51:00 +0000

    [Done] C3D, non_local pytorch version

--- 2_C3D_nonlocal/non_local.py
+++ 2_C3D_nonlocal/non_local.py
@@ -1,54 +1,78 @@
-import tensorflow as tf
-import tensorflow.contrib.slim as slim
+import torch
+from torch import nn
+from torch.nn import functional as F
 
-def NonLocalBlock(input_x, out_channels, sub_sample=True, is_bn=True, scope='NonLocalBlock'):
-    batchsize, clips, height, width, in_channels = input_x.get_shape().as_list()
-    with tf.variable_scope(scope) as sc:
-        with tf.variable_scope('g') as scope:
-            g = slim.conv3d(input_x, out_channels, kernel_size=1, stride=1, scope='g')
-            if sub_sample:
-                g = slim.max_pool3d(g, [1,2,2], stride=[1,2,2], scope='g_max_pool')
 
-        with tf.variable_scope('phi') as scope:
-            '''
-            phi = 
-            '''
-            if sub_sample:
-                phi = slim.max_pool3d(phi, [1,2,2], stride=[1,2,2], scope='phi_max_pool')
+class NonLocalBlock3D(nn.Module):
+    def __init__(self, in_channels, dimension=3, sub_sample=True):
+        super(NonLocalBlock3D, self).__init__()
 
-        with tf.variable_scope('theta') as scope:
-            '''
-            theta = 
-            '''
-        
-        '''
-        g_x = 
-        '''
-        
-        '''
-        theta_x = 
-        '''
-        
-        '''
-        phi_x = 
-        transposed_phi_x = 
-        '''
-        
-        '''
-        f =            # (theta, phi) matrix multiplication
-        f_softmax =    # softmax
-        y =            # (f_softmax, g)
-        y =            # reshape
-        '''
-        
-        with tf.variable_scope('w') as scope:
-            '''
-            w_y =      # Z operation
-            '''
-            if is_bn:
-                w_y = slim.batch_norm(w_y)
-        '''
-        z =           # add y to x
-        '''
-    return z
+        self.dimension = dimension
+        self.sub_sample = sub_sample
 
+        self.in_channels = in_channels
+
+        self.inter_channels = in_channels // 2
+        if self.inter_channels == 0:
+            self.inter_channels = 1
+
+        max_pool_layer = nn.MaxPool3d(kernel_size=(1, 2, 2))
+       
+        self.g = nn.Conv3d(in_channels=self.in_channels, out_channels=self.inter_channels,
+                         kernel_size=1, stride=1, padding=0)
+
+
+        self.W = nn.Conv3d(in_channels=self.inter_channels, out_channels=self.in_channels,
+                         kernel_size=1, stride=1, padding=0)
+        nn.init.constant_(self.W.weight, 0)
+        nn.init.constant_(self.W.bias, 0)
+
+        self.theta = nn.Conv3d(in_channels=self.in_channels, out_channels=self.inter_channels,
+                             kernel_size=1, stride=1, padding=0)
+        
+        self.phi = nn.Conv3d(in_channels=self.in_channels, out_channels=self.inter_channels,
+                           kernel_size=1, stride=1, padding=0)
+
+        if sub_sample:
+            self.g = nn.Sequential(self.g, max_pool_layer)
+            self.phi = nn.Sequential(self.phi, max_pool_layer)
+
+    def forward(self, x):
+        '''
+        :param x: (b, c, t, h, w)
+        :return:
+        '''
+
+        batch_size = x.size(0)
+
+        g_x = self.g(x).view(batch_size, self.inter_channels, -1)
+        g_x = g_x.permute(0, 2, 1)
+
+        theta_x = self.theta(x).view(batch_size, self.inter_channels, -1)
+        theta_x = theta_x.permute(0, 2, 1)
+        
+        phi_x = self.phi(x).view(batch_size, self.inter_channels, -1)
+        
+        f = torch.matmul(theta_x, phi_x)
+        f_div_C = F.softmax(f, dim=-1)
+
+        y = torch.matmul(f_div_C, g_x)
+        y = y.permute(0, 2, 1).contiguous()
+        y = y.view(batch_size, self.inter_channels, *x.size()[2:])
+        W_y = self.W(y)
+        z = W_y + x
+
+        return z
+
+
+
+if __name__ == '__main__':
+    from torch.autograd import Variable
+    import torch
+
+    sub_sample = True
+
+    img = Variable(torch.randn(2, 3, 10, 20, 20))
+    net = NonLocalBlock3D(3, sub_sample=sub_sample)
+    out = net(img)
+    print(out.size())
