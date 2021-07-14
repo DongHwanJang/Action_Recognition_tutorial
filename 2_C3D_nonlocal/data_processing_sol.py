0db989fc39d30997be036f791942e6c2dd386c84
dhjang <jh01120@snu.ac.kr>
2021-07-14 12:49:31 +0900

    tf->torch first commit

--- 2_C3D_nonlocal/data_processing_sol.py
+++ 2_C3D_nonlocal/data_processing_sol.py
@@ -11,6 +11,7 @@ import pickle
 CLIP_LENGTH = 16
 import cv2
 VALIDATION_PRO = 0.2
+from torch.utils.data import Dataset
 
 np_mean = np.load('crop_mean.npy').reshape([CLIP_LENGTH, 112, 112, 3])
 
@@ -18,11 +19,11 @@ def get_test_num(filename):
     lines = open(filename, 'r')
     return len(list(lines))
 
-def get_video_indices(num_vids):
-    video_indices = range(num_vids)
-    random.seed(time.time())
-    shuffled_indices = random.sample(range(num_vids), num_vids)
-    return shuffled_indices
+# def get_video_indices(num_vids):
+#     video_indices = range(num_vids)
+#     random.seed(time.time())
+#     shuffled_indices = random.sample(range(num_vids), num_vids)
+#     return shuffled_indices
 
 def frame_process(clip, clip_length=CLIP_LENGTH, crop_size=112, channel_num=3):
     frames_num = len(clip)
@@ -73,22 +74,49 @@ def convert_images_to_clip(filename, clip_length=CLIP_LENGTH, crop_size=112, cha
     clip = frame_process(clip, clip_length, crop_size, channel_num)
     return clip#shape[clip_length, crop_size, crop_size, channel_num]
 
-def get_batches(data_list, num_classes, batch_index, video_indices, batch_size=10, crop_size=112, channel_num=3):
-    clips = []
-    labels = []
-    video_list = list(data_list)
-    for i in video_indices[batch_index: batch_index + batch_size]:
-        line = video_list[i].strip('\n').split()
+class UCF11Dataset(Dataset):
+    def __init__(self, data_list, num_classes, crop_size=112, channel_num=3):
+        self.data_list = data_list
+        self.video_list = list(data_list)
+        self.crop_size = crop_size
+        self.channel_num = channel_num        
+        self.num_classes = num_classes
+    
+    def __len__(self):
+        return len(self.video_list)
+    
+    def __getitem__(self, i):                
+        line = self.video_list[i].strip('\n').split()
         dirname = line[0]
-        label = data_list[dirname]
-        i_clip = convert_images_to_clip(dirname, CLIP_LENGTH, crop_size, channel_num)
-        clips.append(i_clip)
-        labels.append(int(label))
-    clips = np.array(clips).astype(np.float32)
-    labels = np.array(labels).astype(np.int64)
-    oh_labels = np.zeros([len(labels), num_classes]).astype(np.int64)
-    for i in range(len(labels)):
-        oh_labels[i, labels[i]] = 1
-    batch_index = batch_index + batch_size
-    batch_data = {'clips': clips, 'labels': oh_labels}
-    return batch_data, batch_index
+        label = int(self.data_list[dirname])
+        clips = convert_images_to_clip(dirname, CLIP_LENGTH, self.crop_size, self.channel_num)              
+        
+        clips = np.transpose(np.array(clips).astype(np.float32), (3, 0, 1, 2))
+#         label = np.array(label).astype(np.int64)
+#         oh_labels = np.zeros([1, self.num_classes]).astype(np.int64)        
+#         oh_labels[0, label] = 1
+        
+        batch_data = {'clips': clips, 'labels': label}
+        
+        return batch_data
+        
+
+# def get_batches(data_list, num_classes, batch_index, video_indices, batch_size=10, crop_size=112, channel_num=3):
+#     clips = []
+#     labels = []
+#     video_list = list(data_list)
+#     for i in video_indices[batch_index: batch_index + batch_size]:
+#         line = video_list[i].strip('\n').split()
+#         dirname = line[0]
+#         label = data_list[dirname]
+#         i_clip = convert_images_to_clip(dirname, CLIP_LENGTH, crop_size, channel_num)
+#         clips.append(i_clip)
+#         labels.append(int(label))
+#     clips = np.array(clips).astype(np.float32)
+#     labels = np.array(labels).astype(np.int64)
+#     oh_labels = np.zeros([len(labels), num_classes]).astype(np.int64)
+#     for i in range(len(labels)):
+#         oh_labels[i, labels[i]] = 1
+#     batch_index = batch_index + batch_size
+#     batch_data = {'clips': clips, 'labels': oh_labels}
+#     return batch_data, batch_index
